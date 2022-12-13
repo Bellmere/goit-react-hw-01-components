7ebd3682b32b0f7a9363b85b1d5b0910496659d7
Bellmere <therise00@gmail.com>
2022-12-13 10:06:25 +0200

    add className

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -14,18 +14,18 @@ export default function Profile(props) {
         <p className={css.location}>{location}</p>
       </div>
 
-      <ul class="stats">
-        <li>
-          <span class="label">Followers</span>
-          <span class="quantity">{stats.followers}</span>
+      <ul className={css.stats}>
+        <li className={css.stats__item}>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{stats.followers}</span>
         </li>
-        <li>
-          <span class="label">Views</span>
-          <span class="quantity">{stats.views}</span>
+        <li className={css.stats__item}>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{stats.views}</span>
         </li>
-        <li>
-          <span class="label">Likes</span>
-          <span class="quantity">{stats.likes}</span>
+        <li className={css.stats__item}>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
