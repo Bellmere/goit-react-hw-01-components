e3a8a137281fbc28c4d3e390418db01e39670edb
Bellmere <therise00@gmail.com>
2022-12-13 21:52:56 +0200

    add css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import Profile from "./Profile/Profile";
 import user from './Profile/user.json';
+import Statistics from "./Statistics/Statistics";
+import data from './Statistics/data.json';
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
@@ -19,6 +22,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
     </div>
   );
 };
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,6 +1,34 @@
 import css from '../Statistics/Statistics.module.css';
 import PropTypes from 'prop-types';
 
+function getRandomHexColor() {
+  let bgColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return bgColor;
+}
+
 export default function Statistics(props) {
-    
+  const { title, stats } = props;
+
+  return (
+    <div className={css.statisticsContainer}>
+      <section className={css.statistics}>
+        {title && <h2 className={css.title}>{title}</h2>}
+
+        <ul className={css.statList}>
+          {stats.map(stat => (
+            <li
+              className={title ? css.statListItem : css.statListItemNoTitle}
+              style={{
+                backgroundColor: getRandomHexColor(),
+              }}
+              key={stat.id}
+            >
+              <span class="label">{stat.label}</span>
+              <span class="percentage">{stat.percentage}%</span>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
 }
