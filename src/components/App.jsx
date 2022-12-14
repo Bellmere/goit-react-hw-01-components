296542a2bca130a14eb5bc3292cc0370c6df5121
Bellmere <therise00@gmail.com>
2022-12-14 16:00:53 +0200

    add transaction css+ app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,9 @@ import user from './Profile/user.json';
 import Statistics from "./Statistics/Statistics";
 import data from './Statistics/data.json';
 import FriendList from "./FriendList/FriendList";
-import friends from '../components/FriendList/friends.json'
+import friends from '../components/FriendList/friends.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from './TransactionHistory/transactions.json';
 
 export const App = () => {
   return (
@@ -27,6 +29,7 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
---
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -0,0 +1,39 @@
+import css from '../TransactionHistory/TransactionHistory.module.css';
+import propTypes, { arrayOf } from 'prop-types';
+
+export default function TransactionHistory(props) {
+    const {items} = props;
+
+    return (
+        <table className={css.transactionHistory}>
+        <thead className={css.transactionHeader}>
+            <tr className={css.transactionValue}>
+            <th className={css.rowHeader}>Type</th>
+            <th className={css.rowHeader}>Amount</th>
+            <th className={css.rowHeader}>Currency</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            {items.map(item => 
+                <tr key={item.id} className={css.transactionValue}>
+                <td className={css.transactionDataValue}>{item.type}</td>
+                <td className={css.transactionDataValue}>{item.amount}</td>
+                <td className={css.transactionDataValue}>{item.currency}</td>
+                </tr>     
+            )}
+        </tbody>
+        </table>
+    );
+};
+
+TransactionHistory.propTypes = {
+    items: arrayOf(
+        propTypes.shape({
+            key: propTypes.number.isRequired,
+            type: propTypes.string.isRequired,
+            amount: propTypes.number.isRequired,
+            currency: propTypes.string.isRequired,
+        })
+    ).isRequired,
+}
